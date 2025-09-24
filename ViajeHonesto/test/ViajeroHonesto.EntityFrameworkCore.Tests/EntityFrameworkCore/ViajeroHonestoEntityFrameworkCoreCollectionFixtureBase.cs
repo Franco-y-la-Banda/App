389fc756228d76b0c161834e4030481469586a0a
Wenceslao-x <143463939+Wenceslao-x@users.noreply.github.com>
2025-09-24 17:50:30 -0300

    Revert "sync branches"

--- ViajeHonesto/test/ViajeroHonesto.EntityFrameworkCore.Tests/EntityFrameworkCore/ViajeroHonestoEntityFrameworkCoreCollectionFixtureBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using ViajeroHonesto.EntityFrameworkCore;
-using Xunit;
-
-namespace ViajeroHonesto.EntityFrameworkCore;
-
-public class ViajeroHonestoEntityFrameworkCoreCollectionFixtureBase : ICollectionFixture<ViajeroHonestoEntityFrameworkCoreFixture>
-{
-
-}
