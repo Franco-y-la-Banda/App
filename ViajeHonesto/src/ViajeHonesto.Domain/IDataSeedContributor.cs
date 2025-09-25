da4897c8da39503ae8ebb5394ff39f0fa7185ec6
Wenceslao-x <colasowenceslao@gmail.com>
2025-09-24 21:19:45 -0300

    Feat: DataSeedContributor

---
+++ ViajeHonesto/src/ViajeHonesto.Domain/IDataSeedContributor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using ViajeHonesto.Destinations;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace ViajeHonesto;
+
+public class ViajeHonestoDataSeederContributor
+    : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Destination, Guid> _destinationRepository;
+
+    public ViajeHonestoDataSeederContributor(IRepository<Destination, Guid> destinationRepository)
+    {
+        _destinationRepository = destinationRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _destinationRepository.GetCountAsync() <= 0)
+        {
+            await _destinationRepository.InsertAsync(
+                new Destination
+                {
+                    Name = "Puerto Madero",
+					Country = "Argentina",
+					Region = "Buenos Aires",
+					Population = 5000
+                },
+                autoSave: true
+            );
+
+            await _destinationRepository.InsertAsync(
+                new Destination
+                {
+                    Name = "Concepcion del Uruguay",
+					Country = "Argentina",
+					Region = "Entre Ríos",
+					Population = 300000
+                },
+                autoSave: true
+            );
+        }
+    }
+}
