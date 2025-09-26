9ee10ec10eb238d562f894af84c530d335806280
Franco <148019172+RiolfoF4@users.noreply.github.com>
2025-09-26 16:48:14 -0300

    fix: Syntax

--- ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/IDestinationAppService.cs
+++ ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/IDestinationAppService.cs
@@ -2,15 +2,14 @@
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
-namespace ViajeHonesto.Destinations
-{
-    public interface IDestinationAppService : 
+namespace ViajeHonesto.Destinations;
+
+public interface IDestinationAppService : 
     ICrudAppService< //Defines CRUD methods
         DestinationDto, //Used to show Destinations
         Guid, //Primary key of the Destination entity
         PagedAndSortedResultRequestDto, //Used for sorting
         CreateUpdateDestinationDto> //Used to create/update a Destination
-    {
+{
 
-    }
 }
