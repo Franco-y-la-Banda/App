adad1510109601f5ca8f71abbeb96597d12adc4a
Franco <148019172+RiolfoF4@users.noreply.github.com>
2025-10-15 15:50:36 -0300

    refactor(api): Decouple GeoDbCitySearchService from HTTP client

---
+++ ViajeHonesto/src/ViajeHonesto.Application/Destination/GeoDbApiClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+
+namespace ViajeHonesto.Destinations
+{
+    public class GeoDbApiClient : IGeoDbApiClient, ITransientDependency
+    {
+        private static readonly string apiKey = "0d591376bamsh69ea0c8ddcb541ep152145jsn345c066e6f52";
+        private static readonly string baseUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+        public async Task<string> SearchCitiesRawAsync(string partialCityName, int limit)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Configuración de Headers
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
+                client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com");
+
+                // Endpoint con query params
+                string url = $"{baseUrl}/cities?namePrefix={Uri.EscapeDataString(partialCityName)}&limit={limit}";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResult = await response.Content.ReadAsStringAsync();
+                    return jsonResult;
+                }
+                else
+                {
+                    throw new Exception("Error fetching city data");
+                }
+            }
+        }
+    }
+}
--- ViajeHonesto/src/ViajeHonesto.Application/Destination/GeoDbCitySearchService.cs
+++ ViajeHonesto/src/ViajeHonesto.Application/Destination/GeoDbCitySearchService.cs
@@ -11,15 +11,19 @@ namespace ViajeHonesto.Destinations
 {
     public class GeoDbCitySearchService : ICitySearchService, ITransientDependency
     {
-        private static readonly string apiKey = "0d591376bamsh69ea0c8ddcb541ep152145jsn345c066e6f52";
         private static readonly int resultLimit = 5;
-        private static readonly string baseUrl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+        private readonly IGeoDbApiClient _geoDbApiClient;
+        public GeoDbCitySearchService(IGeoDbApiClient geoDbApiClient)
+        {
+            _geoDbApiClient = geoDbApiClient;
+        }
 
         public async Task<CitySearchResultDto> SearchCitiesByNameAsync(CitySearchRequestDto request)
         {
-            // https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/overview
-            var jsonResult = JsonDocument.Parse(await SearchCities(request));
-            var jsonCities = jsonResult.RootElement.GetProperty("data");
+            var jsonRaw = await _geoDbApiClient.SearchCitiesRawAsync(request.PartialCityName, resultLimit);
+            var jsonDocument = JsonDocument.Parse(jsonRaw);
+            var jsonCities = jsonDocument.RootElement.GetProperty("data");
 
             var citySearchResultDto = new CitySearchResultDto();
             foreach (var city in jsonCities.EnumerateArray())
@@ -33,31 +37,6 @@ namespace ViajeHonesto.Destinations
             }
 
             return citySearchResultDto;
-        }
-
-        static async Task<string> SearchCities(CitySearchRequestDto request)
-        {
-            using (HttpClient client = new HttpClient())
-            {
-                // Configuración de Headers
-                client.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
-                client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wft-geo-db.p.rapidapi.com");
-
-                // Endpoint con query params
-                string url = $"{baseUrl}/cities?namePrefix={Uri.EscapeDataString(request.PartialCityName)}&limit={resultLimit}";
-
-                HttpResponseMessage response = await client.GetAsync(url);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    string jsonResult = await response.Content.ReadAsStringAsync();
-                    return jsonResult;
-                }
-                else
-                {
-                    throw new Exception("Error fetching city data");
-                }
-            }
         }
     }
 }
