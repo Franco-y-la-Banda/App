407ef9afc59ff66382367314de1c1e217f1c190e
Franco <148019172+RiolfoF4@users.noreply.github.com>
2025-10-31 18:11:48 -0300

    feat(seeding): add master data seeder with ordered execution

---
+++ ViajeHonesto/src/ViajeHonesto.Domain/DestinationsDataSeederContributor.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ViajeHonesto.Destinations;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using ViajeHonesto.Constants;
+
+namespace ViajeHonesto;
+
+public class DestinationsDataSeederContributor : ITransientDependency
+{
+    private readonly IRepository<Destination, Guid> _destinationRepository;
+
+    public DestinationsDataSeederContributor(IRepository<Destination, Guid> destinationRepository)
+    {
+        _destinationRepository = destinationRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _destinationRepository.GetCountAsync() <= 0)
+        {
+            var guidIndex = 0;
+
+            var destination = new Destination(TestGuids.DestinationGuid(guidIndex))
+            {
+                Name = "Puerto Madero",
+                Country = "Argentina",
+                Region = "Buenos Aires",
+                Population = 5000,
+                Coordinate = new Coordinate(-34.6083f, -58.3636f)
+            };
+
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/puerto-madero/puente-de-la-mujer.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/puerto-madero/dique-de-noche.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/puerto-madero/skyline-rio.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/puerto-madero/restaurantes-costanera.jpg");
+
+
+            await _destinationRepository.InsertAsync(
+                destination,
+                autoSave: true
+            );
+
+            guidIndex++;
+
+            destination = new Destination(TestGuids.DestinationGuid(guidIndex))
+            {
+                Name = "Concepción del Uruguay",
+                Country = "Argentina",
+                Region = "Entre Ríos",
+                Population = 300000,
+                Coordinate = new Coordinate(-32.4833f, -58.2333f)
+            };
+
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/concepcion-del-uruguay/ramblas-costanera.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/concepcion-del-uruguay/plaza-ramirez.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/concepcion-del-uruguay/palacio-san-jose.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/concepcion-del-uruguay/playa-banco-pelay.jpg");
+
+            await _destinationRepository.InsertAsync(
+                destination,
+                autoSave: true
+            );
+
+            guidIndex++;
+
+            destination = new Destination(TestGuids.DestinationGuid(guidIndex))
+            {
+                Name = "Villa La Angostura",
+                Country = "Argentina",
+                Region = "Neuquén",
+                Population = 14000,
+                Coordinate = new Coordinate(-40.7617f, -71.6461f)
+            };
+
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/villa-la-angostura/bosque-arrayanes.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/villa-la-angostura/lago-nahuel-huapi.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/villa-la-angostura/centro-invierno.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/villa-la-angostura/mirador-belvedere.jpg");
+
+            await _destinationRepository.InsertAsync(
+                destination,
+                autoSave: true
+            );
+
+            guidIndex++;
+
+            destination = new Destination(TestGuids.DestinationGuid(guidIndex))
+            {
+                Name = "Tilcara",
+                Country = "Argentina",
+                Region = "Jujuy",
+                Population = 8000,
+                Coordinate = new Coordinate(-23.5733f, -65.3928f)
+            };
+
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/tilcara/pucara-de-tilcara.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/tilcara/quebrada-humahuaca.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/tilcara/mercado-artesanal.jpg");
+            destination.AddPhoto(Guid.NewGuid(), "/images/destinations/tilcara/callecitas-colores.jpg");
+
+            await _destinationRepository.InsertAsync(
+                destination,
+                autoSave: true
+            );
+
+            guidIndex++;
+        }
+    }
+}
---
+++ ViajeHonesto/src/ViajeHonesto.Domain/ReviewsDataSeederContributor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using ViajeHonesto.Constants;
+using ViajeHonesto.Reviews;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Identity;
+using Volo.Abp.Modularity;
+
+namespace ViajeHonesto;
+
+class ReviewsDataSeederContributor: ITransientDependency
+{
+    private readonly IRepository<Review> _reviewRepository;
+
+    public ReviewsDataSeederContributor(IRepository<Review> reviewRepository)
+    {
+        _reviewRepository = reviewRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _reviewRepository.GetCountAsync() <= 0)
+        {
+            var review = new Review(
+            TestGuids.DestinationGuid(0),
+            TestGuids.UserGuid(0),
+            new Rating(5),
+            new CComment("Muy buena"));
+            await _reviewRepository.InsertAsync(review, autoSave: true);
+
+            review = new Review(
+                TestGuids.DestinationGuid(1),
+                TestGuids.UserGuid(1),
+                new Rating(4),
+                new CComment("Hermosa vista y buena comida"));
+            await _reviewRepository.InsertAsync(review, autoSave: true);
+
+            review = new Review(
+                TestGuids.DestinationGuid(2),
+                TestGuids.UserGuid(2),
+                new Rating(3),
+                new CComment("Lindo lugar pero algo caro"));
+            await _reviewRepository.InsertAsync(review, autoSave: true);
+
+            review = new Review(
+                TestGuids.DestinationGuid(3),
+                TestGuids.UserGuid(0),
+                new Rating(5),
+                new CComment("Una experiencia inolvidable"));
+            await _reviewRepository.InsertAsync(review, autoSave: true);
+
+            review = new Review(
+                TestGuids.DestinationGuid(1),
+                TestGuids.UserGuid(2),
+                new Rating(4),
+                new CComment("Ideal para relajarse un fin de semana"));
+            await _reviewRepository.InsertAsync(review, autoSave: true);
+        }
+    }
+}
