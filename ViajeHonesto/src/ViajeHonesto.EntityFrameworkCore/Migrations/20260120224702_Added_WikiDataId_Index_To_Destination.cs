369b930c894680ae3baedef1e7f104249b8f2c66
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-01-20 19:56:10 -0300

    feat(destination): Add WikiDataId fiel to DB and make migration

---
+++ ViajeHonesto/src/ViajeHonesto.EntityFrameworkCore/Migrations/20260120224702_Added_WikiDataId_Index_To_Destination.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ViajeHonesto.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_WikiDataId_Index_To_Destination : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WikiDataId",
+                table: "AppDestinations",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppDestinations_WikiDataId",
+                table: "AppDestinations",
+                column: "WikiDataId",
+                unique: true,
+                filter: "[WikiDataId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AppDestinations_WikiDataId",
+                table: "AppDestinations");
+
+            migrationBuilder.DropColumn(
+                name: "WikiDataId",
+                table: "AppDestinations");
+        }
+    }
+}
