d6df545e6ab74d608a5e9c7cc5a02a1815267616
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-01-22 20:57:55 -0300

    feat(destination): Add DTOs for city search details

---
+++ ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/CityDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ViajeHonesto.Destinations
+{
+    public class CityDetailsDto
+    {
+        public string? WikiDataId { get; set; }
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public string? Region { get; set; }
+        public long Population { get; set; }
+        public CoordinateDto Coordinate { get; set; } = new CoordinateDto { };
+        public DestinationPhotoDto[] Photos { get; set; } = [];
+
+        public bool IsSaved { get; set; }
+        public Guid? LocalId { get; set; }
+    }
+}
