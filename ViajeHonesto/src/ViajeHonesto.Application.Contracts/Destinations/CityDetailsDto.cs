38e520ab371a4bb3232ea45fbf3efa69a7137601
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-01-23 00:03:15 -0300

    feat(destination): Make Name, Country and Region mandatory fields in DTOs

--- ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/CityDetailsDto.cs
+++ ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/CityDetailsDto.cs
@@ -5,9 +5,9 @@ namespace ViajeHonesto.Destinations
     public class CityDetailsDto
     {
         public string? WikiDataId { get; set; }
-        public string? Name { get; set; }
-        public string? Country { get; set; }
-        public string? Region { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
         public long Population { get; set; }
         public CoordinateDto Coordinate { get; set; } = new CoordinateDto { };
         public DestinationPhotoDto[] Photos { get; set; } = [];
--- ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/CityDto.cs
+++ ViajeHonesto/src/ViajeHonesto.Application.Contracts/Destinations/CityDto.cs
@@ -3,9 +3,9 @@
     public class CityDto
     {
         public string? WikiDataId { get; set; }
-        public string? Name { get; set; }
-        public string? Country { get; set; }
-        public string? Region { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
         public long Population { get; set; }
     }
 }
