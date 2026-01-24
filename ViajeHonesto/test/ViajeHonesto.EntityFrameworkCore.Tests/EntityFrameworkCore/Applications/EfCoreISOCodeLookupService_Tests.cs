5cddee524b8a52ef1e4284764cba6e696a796f23
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-01-24 20:34:55 -0300

    feat: Add tests

---
+++ ViajeHonesto/test/ViajeHonesto.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/EfCoreISOCodeLookupService_Tests.cs
@@ -0,0 +1,8 @@
+﻿using ViajeHonesto.Destinations;
+
+namespace ViajeHonesto.EntityFrameworkCore.Applications
+{
+    public class EfCoreISOCodeLookupService_Tests : ISOCodeLookupService_Tests<ViajeHonestoEntityFrameworkCoreTestModule>
+    {
+    }
+}
