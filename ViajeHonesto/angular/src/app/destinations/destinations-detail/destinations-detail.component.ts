399f30e339f855981120773e002d9ded35e139cd
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-02-18 21:07:47 -0300

    feat: Add first layout

--- ViajeHonesto/angular/src/app/destinations/destinations-detail/destinations-detail.component.ts
+++ ViajeHonesto/angular/src/app/destinations/destinations-detail/destinations-detail.component.ts
@@ -3,22 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { DestinationService } from '../../proxy/destinations/destination.service';
 import { CityDetailsDto, CityDetailsSearchRequestDto } from 'src/app/proxy/destinations';
+import { CoreModule, SessionStateService } from '@abp/ng.core';
 
 @Component({
   selector: 'app-destination-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CoreModule],
   templateUrl: './destinations-detail.component.html',
   styleUrls: ['./destinations-detail.component.scss'],
 })
-
 export class DestinationsDetailComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly destinationService = inject(DestinationService);
+  private sessionState = inject(SessionStateService);
 
   destination: CityDetailsDto = null;
-  request: CityDetailsSearchRequestDto = {wikiDataId: null};
+  request: CityDetailsSearchRequestDto = { wikiDataId: null };
   loading = true;
+  language: string;
 
   ngOnInit(): void {
     this.request.wikiDataId = this.route.snapshot.paramMap.get('id');
@@ -26,6 +28,7 @@ export class DestinationsDetailComponent implements OnInit {
     if (this.request.wikiDataId) {
       this.loadDetails(this.request.wikiDataId);
     }
+    this.language = this.sessionState.getLanguage().substring(0, 2);
   }
 
   private loadDetails(id: string): void {
