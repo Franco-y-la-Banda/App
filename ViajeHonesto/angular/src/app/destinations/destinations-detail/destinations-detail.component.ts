e3b5b54c3e9e422ff4474bac53f92884cb3a1052
Franco <148019172+RiolfoF4@users.noreply.github.com>
2026-02-19 20:34:23 -0300

    feat: Add embeded map

--- ViajeHonesto/angular/src/app/destinations/destinations-detail/destinations-detail.component.ts
+++ ViajeHonesto/angular/src/app/destinations/destinations-detail/destinations-detail.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { DestinationService } from '../../proxy/destinations/destination.service';
-import { CityDetailsDto, CityDetailsSearchRequestDto } from 'src/app/proxy/destinations';
+import {
+  CityDetailsDto,
+  CityDetailsSearchRequestDto,
+  CoordinateDto,
+} from 'src/app/proxy/destinations';
 import { CoreModule, SessionStateService } from '@abp/ng.core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-destination-detail',
@@ -16,11 +21,14 @@ export class DestinationsDetailComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly destinationService = inject(DestinationService);
   private sessionState = inject(SessionStateService);
+  private readonly sanitizer = inject(DomSanitizer);
 
   destination: CityDetailsDto = null;
   request: CityDetailsSearchRequestDto = { wikiDataId: null };
   loading = true;
   language: string;
+  embedMapUrl: SafeResourceUrl | null = null;
+  mapUrl: SafeUrl | null = null;
 
   ngOnInit(): void {
     this.request.wikiDataId = this.route.snapshot.paramMap.get('id');
@@ -28,6 +36,7 @@ export class DestinationsDetailComponent implements OnInit {
     if (this.request.wikiDataId) {
       this.loadDetails(this.request.wikiDataId);
     }
+
     this.language = this.sessionState.getLanguage().substring(0, 2);
   }
 
@@ -38,12 +47,21 @@ export class DestinationsDetailComponent implements OnInit {
       next: result => {
         this.destination = result;
         this.loading = false;
+
+        if (this.destination.coordinate) {
+          this.loadMap(this.destination.coordinate);
+        }
       },
       error: err => {
         console.error(err);
         this.loading = false;
       },
     });
+  }
+
+  private loadMap(coord: CoordinateDto): void {
+    this.embedMapUrl = this.getMapUrl(coord);
+    this.mapUrl = this.getGoogleMapsLink(coord);
   }
 
   goBack(): void {
@@ -56,5 +74,17 @@ export class DestinationsDetailComponent implements OnInit {
         window.location.href = window.location.origin + '/destinations';
       }
     }, 500);
+  }
+
+  getMapUrl(coords: CoordinateDto): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://maps.google.com/maps?q=${coords.latitude},${coords.longitude}&z=12&output=embed`,
+    );
+  }
+
+  getGoogleMapsLink(coords: CoordinateDto): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(
+      `https://www.google.com/maps/search/?api=1&query=${coords.latitude},${coords.longitude}`,
+    );
   }
 }
